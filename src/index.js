82a7a223b423bc85381088b3cd1c75f9d882950a
volha bukhal <o.bukhal@gmail.com>
2021-08-04 22:16:03 +0300

    feat: add reverse str

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+    let arr = n.toString().split('');
+    let newStr = '';
+
+    arr = arr.filter( el => el !== "-");
+
+    for (let i = arr.length-1; i >= 0; i--) {
+        newStr += arr[i];
+    }
+
+    return +newStr;
+
 }
